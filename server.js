d632fbc62a0a4bff9a8febc8d8e75cac3915f497
Anil Yadav <iter.anilyadav@gmail.com>
2020-11-27 11:53:28 +0530

    updated

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const port = 3000;
 const path = require('path');
-const publicPath = path.join(__dirname, '/../public');
+const publicPath = path.join(__dirname, 'public');
 var app = express();
 app.use(express.static(publicPath));
+
 app.listen(port, () => {
     console.log("Server is Running..." + port);
 })
