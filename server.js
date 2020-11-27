6020f98120e4c639ecf25e361b544cd4fa9deeed
Anil Yadav <iter.anilyadav@gmail.com>
2020-11-27 12:21:57 +0530

    updated

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const http = require('http');
 const port = process.env.PORT || 3000;
 const path = require('path');
+const socketIO = require('socket.io')
 const publicPath = path.join(__dirname, 'public');
-var app = express();
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+//public url folder
 app.use(express.static(publicPath));
-
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log("A new user connected");
+    socket.on('disconnect', () => {
+        console.log("user was disconnected");
+    })
+});
+//server access
+server.listen(port, () => {
     console.log("Server is Running..." + port);
 })
