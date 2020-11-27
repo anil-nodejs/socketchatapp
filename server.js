2fc9d491e197e3c06d3c57c017529b776ca41c04
Anil Yadav <iter.anilyadav@gmail.com>
2020-11-27 16:14:40 +0530

    added

--- server.js
+++ server.js
@@ -3,18 +3,29 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 const path = require('path');
 const socketIO = require('socket.io')
+//access the public path
 const publicPath = path.join(__dirname, 'public');
 let app = express();
 let server = http.createServer(app);
 let io = socketIO(server);
 //public url folder
 app.use(express.static(publicPath));
+
+//connecttion events
 io.on('connection', (socket) => {
     console.log("A new user connected");
     socket.emit("newMessage", {
-        from: "Mike",
-        text: "Hi, How r you!"
+        from: "Admin",
+        text: "Welcome to the SAY Chat App!",
+        createdAt: new Date().getTime()
     });
+
+    socket.broadcast.emit("newMessage", {
+        from: "Admin",
+        text: "New user joined!",
+        createdAt: new Date().getTime()
+    });
+    //create event
     socket.on('createMessage', (message) => {
         console.log("created Message", message);
         io.emit('newMessage',
@@ -22,12 +33,21 @@ io.on('connection', (socket) => {
                 form: message.from,
                 text: message.text,
                 createdAt: new Date().getTime()
-            })
+            });
+        // socket.broadcast.emit(
+        //     {
+        //         form: message.from,
+        //         text: message.text,
+        //         createdAt: new Date().getTime()
+        //     });
     });
+
+    //disconnect event
     socket.on('disconnect', () => {
         console.log("user was disconnected");
     });
 });
+
 //server access
 server.listen(port, () => {
     console.log("Server is Running..." + port);
