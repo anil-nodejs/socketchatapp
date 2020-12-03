2d47740b8113289a436a2e6839c13bec2f7ddf92
Anil Yadav <iter.anilyadav@gmail.com>
2020-12-03 14:02:11 +0530

    UI updated

--- utils/message.js
+++ utils/message.js
@@ -1,17 +1,17 @@
+const moment = require('moment');
 let generateMessage = (from, text) => {
     return {
         from,
         text,
-        createdAt: new Date().getTime()
+        createdAt: moment().valueOf()
     }
 };
-let generateLocationMessage = (from, lat, lng) =>
-{
+let generateLocationMessage = (from, lat, lng) => {
     return {
         from,
         url: `https://www.google.com/maps?q=${lat},${lng}`,
-        createdAt: new Date().getTime()
+        createdAt: moment().valueOf()
     }
-    }
+}
 
 module.exports = { generateMessage, generateLocationMessage }
